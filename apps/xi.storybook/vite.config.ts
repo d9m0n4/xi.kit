8c67628f49e17608db8113cffffb30a0398bf1de
unknown <ched08@yandex.ru>
2025-01-26 21:15:23 +0300

    fix package import

--- apps/xi.storybook/vite.config.ts
+++ apps/xi.storybook/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
-    preserveSymlinks: true,
+    preserveSymlinks: true
   },
-
+  optimizeDeps: {
+    include: ['@xipkg/button']
+  }
 });
