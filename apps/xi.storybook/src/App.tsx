7a82a336349dd68742b854e92a375e4a7cfd2cfd
unknown <ched08@yandex.ru>
2025-01-26 12:05:24 +0300

    fix t config

--- apps/xi.storybook/src/App.tsx
+++ apps/xi.storybook/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@xipkg/button";
+
 function App() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <h1>xi.effect storybook</h1>
+      <Button>123123</Button>
     </div>
   );
 }
